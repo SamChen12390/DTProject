91565dfac5bd30728cbea9badd03acbde4507150
Shukun Lin <42133373+rmit-s3587391-Shukun-Lin@users.noreply.github.com>
2020-08-23 15:54:18 +1000

    Basic
    Push the /Assets and /ProjectSettings folder - with basic AR integration.

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}

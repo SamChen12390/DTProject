c5f2fc34e4228471fb6f091f45c518d921385065
Shukun Lin <42133373+rmit-s3587391-Shukun-Lin@users.noreply.github.com>
2020-09-11 20:23:06 +1000

    Before meger

---
+++ Assets/Archer/Scripts/Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float maxHealth;
+    public float attackDamage;
+    public HealthBar healthBar;
+    public ShotEffect effect1;
+    public ShotEffect effect2;
+    public AE_AnimatorEvents aE_AnimatorEvents;
+
+    private void Start()
+    {
+        if (transform.CompareTag("Archer"))
+        {
+            if (transform.gameObject.layer == LayerMask.NameToLayer("Blue team"))
+            {
+                aE_AnimatorEvents.Effect1.Prefab = effect1._effect1;
+                aE_AnimatorEvents.Effect2.Prefab = effect1._effect2;
+            }
+            if (transform.gameObject.layer == LayerMask.NameToLayer("Red team"))
+            {
+                aE_AnimatorEvents.Effect1.Prefab = effect2._effect1;
+                aE_AnimatorEvents.Effect2.Prefab = effect2._effect2;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class ShotEffect
+    {
+        public GameObject _effect1;
+        public GameObject _effect2;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.parent.gameObject.CompareTag("Arrow"))
+        {
+            healthBar.TakeDamage(attackDamage);
+            Destroy(collision.transform.parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Archer/Scripts/Launcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject characterArcher;
+    public GameObject defenseSpawnPoint;
+    public GameObject defensePoints;
+    public GameObject enemySpawnPoints;
+    private int currentDefenseUnit = 1;
+    private int maxDefenseUnit = 0;
+    private int currentEnemyUnit = 1;
+    private int maxEnemyUnit = 0;
+
+    private void Start()
+    {
+        maxEnemyUnit = enemySpawnPoints.GetComponentsInChildren<Transform>().Length;
+        maxDefenseUnit = defensePoints.GetComponentsInChildren<Transform>().Length;
+
+        StartCoroutine(DelaySpawn(2f, "Red team"));
+        StartCoroutine(DelaySpawn(2f, "Blue team"));
+    }
+
+    public void GenerateCharacterForBlueTeam(GameObject characterPrefabs, Vector3 spawnPosition, Transform destination)
+    {
+        GameObject newCharater = Instantiate(characterPrefabs, spawnPosition + Vector3.up, Quaternion.identity);
+        newCharater.layer = LayerMask.NameToLayer("Blue team");
+        newCharater.GetComponent<NavMeshController>().SetTarget(destination);
+    }
+    public void GenerateCharacterForRedTeam(GameObject characterPrefabs, Vector3 position, Transform destination)
+    {
+        GameObject newCharater = Instantiate(characterPrefabs, position + Vector3.up, Quaternion.identity);
+        newCharater.layer = LayerMask.NameToLayer("Red team");
+        newCharater.GetComponent<NavMeshController>().SetTarget(destination);
+    }
+
+    IEnumerator DelaySpawn(float delayTime, string teamName)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delayTime);
+            if (teamName.Contains("Red team") && currentEnemyUnit < maxEnemyUnit)
+            {
+                GenerateCharacterForRedTeam(characterArcher, enemySpawnPoints.GetComponentsInChildren<Transform>()[currentEnemyUnit].position, defenseSpawnPoint.transform);
+                currentEnemyUnit++;
+            }
+            else if (teamName.Contains("Blue team") && currentDefenseUnit < maxDefenseUnit)
+            {
+                GenerateCharacterForBlueTeam(characterArcher, defenseSpawnPoint.transform.position, defensePoints.GetComponentsInChildren<Transform>()[currentDefenseUnit]);
+                currentDefenseUnit++;
+            }
+        }
+    }
+}
